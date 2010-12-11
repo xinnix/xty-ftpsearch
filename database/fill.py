c1f0c0fd727944de8057630ca9d78c8946f03348
xiguagua <root.xigua@gmail.com>
2010-12-11 15:29:56 +0800

    1.1, improve version

--- database/fill.py
+++ database/fill.py
@@ -11,6 +11,7 @@ class FtpIndex:
     def __init__(self):
         self.cur = connect(host='localhost', user='root', passwd='xinxin', db='ftpsearch', charset='utf8').cursor()
         self.querycol = ('id', 'site', 'port', 'user', 'pw')
+        self.firstdir = []
         
     def fill(self):
         querystr = 'select %s, %s, %s, %s, %s from ftpinfo where indb=0'% self.querycol
@@ -28,51 +29,79 @@ class FtpIndex:
         except:
             print 'something wrong happened, maybe the url is wrong!'
         else:
-            self.startindex('')
-            self.ftp.close()
+            # 先索引根目录,这样在以后的递归索引中就不用判断是否为根目录
+            self.indexroot()
+
+            for node in self.firstdir:
+                self.indexsubnode(node)
+            self.ftp.abort()
             self.cur.execute("update ftpinfo set indb=1 where id="+str(self.site['id'])) 
             self.cur.execute("commit")
-            
-    def startindex(self, directory): # directory是绝对路径
-        print 'indexing %s' % directory
-        if not directory: #根目录
-            self.cur.execute("insert into cat (ipid) values (%s)" % \
-                       (self.site['id']))
-        elif '/' not in directory: #第一层目录
-            self.cur.execute("select id from cat where cat is null and ipid="+str(self.site['id']))
-            pid = self.cur.fetchone()
-            self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
-                            (directory, pid[0], self.site['id']))
-        else:
-            dirs = directory.split('/') #切片
-            self.cur.execute("select id from cat where cat='"+dirs[-2]+"' and ipid="+str(self.site['id']))
-            pid = self.cur.fetchone()
-            self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
-                            (dirs[-1], pid[0], self.site['id']))
-        
-        for d in self.ftp.nlst(directory):    
-            try:
-                if d != self.ftp.nlst(d)[0]: #d是目录
-                    self.startindex(d)
-                else: #d是文件
-                    if '/' not in d: #根目录下的文件
-                        self.cur.execute("select id from cat where cat is null and ipid="+str(self.site['id']))
-                        pid = self.cur.fetchone()
-                        self.cur.execute("insert into files (file, postfix, pid, ipid) \
-                            values ('%s', '%s', %s, %s)" % \
-                            (d, d.split('.')[-1], pid[0], self.site['id']))
-                    else:
-                        dirs = d.split('/')
-                        self.cur.execute("select id from cat where cat='"+dirs[-2]+"' and ipid="+str(self.site['id']))
-                        pid = self.cur.fetchone()
-                        self.cur.execute("insert into files (file, postfix, pid, ipid) \
-                            values ('%s', '%s', %s, %s)" % \
-                            (dirs[-1], dirs[-1].split('.')[-1], pid[0], self.site['id']))
-            except: #d是空目录
-                pass
-   
 
-        
+    def indexroot(self):
+        self.cur.execute("insert into cat (ipid) values (%s)" % self.site['id'])
+        for node in self.ftp.nlst():
+            subnode = self.ftp.nlst(node)
+            self.cur.execute("select id from cat where cat is null and ipid=%s" % self.site['id'])
+            pid = self.cur.fetchone()
+            if len(subnode) == 0: # node为空目录
+                self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
+                                 (node, pid[0], self.site['id']))
+
+            elif len(subnode) > 1: # node为多文件目录
+                self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
+                                 (node, pid[0], self.site['id']))
+                self.firstdir.append(node)
+                
+            elif subnode[0] != node: # node为单文件目录
+                self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
+                                 (node, pid[0], self.site['id']))
+                self.firstdir.append(node)
+                
+            else: # node为文件
+                if '.' in node: # 文件有后缀
+                    self.cur.execute("insert into files (file, postfix, pid, ipid)\
+                                    values ('%s', '%s', %s, %s)" % \
+                                    (node, node.split('.')[-1], pid[0], self.site['id']))
+                else: 
+                    self.cur.execute("insert into files (file, pid, ipid)\
+                                    values ('%s', %s, %s)" % \
+                                    (node, pid[0], self.site['id']))
+
+                        
+    def indexsubnode(self, directory): # directory是绝对路径
+        print 'indexing %s' % directory
+
+        for node in self.ftp.nlst(directory):
+            subnode = self.ftp.nlst(node)
+            dirs = node.split('/') #切片
+            self.cur.execute("select id from cat where cat='%s' and ipid=%s" % (dirs[-2], self.site['id']))
+            pid = self.cur.fetchone()
+            if len(subnode) == 0: # node为空目录
+                self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
+                                (dirs[-1], pid[0], self.site['id']))
+
+            elif len(subnode) > 1: # node为多文件目录
+                self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
+                                (dirs[-1], pid[0], self.site['id']))
+                self.indexsubnode(node)
+
+            elif subnode[0] != node: # node为单文件目录
+                self.cur.execute("insert into cat (cat, pid, ipid) values ('%s', %s, %s)" % \
+                                 (dirs[-1], pid[0], self.site['id']))
+                self.indexsubnode(node)
+
+            else: # node为文件
+                if '.' in node: # 文件有后缀
+                     # print dirs[-1]
+                    self.cur.execute("insert into files (file, postfix, pid, ipid)\
+                                    values ('%s', '%s', %s, %s)" % \
+                                    (dirs[-1], dirs[-1].split('.')[-1], pid[0], self.site['id']))
+                else: 
+                    self.cur.execute("insert into files (file, pid, ipid)\
+                                    values ('%s', %s, %s)" % \
+                                    (dirs[-1], pid[0], self.site['id']))
+
 if __name__ == '__main__':
     ftpindex = FtpIndex()
     ftpindex.fill()
