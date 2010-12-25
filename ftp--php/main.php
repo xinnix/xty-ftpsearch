42cdc7a7099bef6c263cc58933afb0fb02908597
yuanmu <xian--zhangyijun@163.com>
2010-12-25 10:26:21 +0800

    2010年12月25日上午10:30，调试通过,分页显示顺序是先文件名 后目录名
    Signed-off-by: yuanmu <xian--zhangyijun@163.com>

--- ftp--php/main.php
+++ ftp--php/main.php
@@ -1,14 +1,14 @@
-<html>
+ï»¿<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>¿ì¿ìËÑ----</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>æœç´¢</title>
 </head>
 <form name="login" method="post" action="search.php">
 <table border="0" cellPadding="0" cellSpacing="0" height="100%" width="100%">
   <tr>
     <td height="30" align="center" valign="middle">
     <input type="text" name="keyword" size="20"/>
-    <input type="submit" name="submit" value="ËÑË÷"/></td>
+    <input type="submit" name="submit" value="æœç´¢"/></td>
   </tr>
 </table>
 </form>
