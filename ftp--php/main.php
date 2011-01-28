1476d1db07210befe2ae223b118393c977cd63d3
xinnix <xinnix2000@163.com>
2011-01-28 22:53:15 +0800

    对主界面进行个美化

--- ftp--php/main.php
+++ ftp--php/main.php
@@ -1,10 +1,12 @@
 ﻿<html>
 <head>
+<link rel="stylesheet" href="css/main.css" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>搜索</title>
 </head>
 <div class="title"><H1>未知搜索</H1></div>
 <div class="link"><a href='addftp.php'>手动添加ftp服务器</a></div>
+<div class-"logo"></div>
 <form name="login" method="post" action="search.php">
 <table border="0" cellPadding="0" cellSpacing="0" height="100%" width="100%">
   <tr>
